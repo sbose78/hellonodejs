f3062db45741ed78d94fd0b4c258a4b195f6c89b
unknown <Sonali@Dreamz.(none)>
2011-12-14 22:51:35 +0530

    commitjoyent2

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 
-var http = require('http');
+var http = require('http') sys = require('util'),      fs = require('fs'),    index;
+
+
+fs.readFile('./hi.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    index = data;
+});
+ 
  
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.writeHead(200, { "Content-Type": "text/plain" });
+     res.write(index);
   res.end("Hello world\n");
 });
  
