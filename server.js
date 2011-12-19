9ff21372b57f5f6366d2077b905ac6e99fbc806c
unknown <Sonali@Dreamz.(none)>
2011-12-20 01:04:21 +0530

    request -handler

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 
 var http = require('http'), sys = require('util'),      fs = require('fs'),    index;
 
-
+/*
 fs.readFile('./hi.html', function (err, data) {
     if (err) {
         throw err;
     }
     index = data;
 });
- 
+ */
  
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-     res.write(index);
-  res.end();
+
+	var pathname = url.parse(req.url).pathname;
+     res.writeHead(200, { "Content-Type": "text/html" });
+     //res.write(index);
+	  res.write("Request for " + pathname + " received.");
+	 
+     res.end();
 });
  
 server.listen(process.env.PORT || 8001);
