8443fc998d278031d58fc8c7da5f480a08a97bb9
unknown <Sonali@Dreamz.(none)>
2011-12-20 01:45:10 +0530

    routers, getfiles12

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ var server = http.createServer(function (req, res) {
      res.writeHead(200, { "Content-Type": "text/html" });
      //res.write(index);
 	 var ans= routers.route(pathname);
-	  res.write("Request for " + pathname + " received.----" +ans +"something");
-	 
+	  res.write("Request for " + pathname + " received.----" +ans +"something"); 
      res.end();
 });
  
